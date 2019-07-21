9208eaa90c030fb99498aac0bdc37ea209de42ba
LightDiscord <root@arnaud.sh>
2019-07-21 21:50:25 +0200

    feat: add description credit

--- chip8-core/src/instructions.rs
+++ chip8-core/src/instructions.rs
@@ -39,6 +39,10 @@ macro_rules! instructions {
             })*
         ),*
     ) => {
+        /// Enum containing all chip8 instructions.
+        ///
+        /// Description of each instruction retrieved from the
+        /// [Cowngod's Chip-8 Technical Reference v1.0](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
         pub enum Instruction {
             $(
                 $(#[$meta])*
